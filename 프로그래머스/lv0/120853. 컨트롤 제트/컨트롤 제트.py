5bfed4de2adffe4b1256b8dda23a7d36f56f0896
LeeByoungho1 <121912439+LeeByoungho1@users.noreply.github.com>
2023-04-03 10:17:38 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = 0
+    l = s.split(" ")
+    print(l)
+    for i in range(len(l)):
+        if l[i] == "Z":
+            answer -= int(l[i-1])
+        else:
+            answer += int(l[i])
+    return answer
